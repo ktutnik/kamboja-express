af42d0b53befb22a7fc7254d4fef253e43549da4
Ketut Sandiarsa <ktutnik@gmail.com>
2017-02-15 09:22:14 +0800

    Use kamboja 0.0.1-6

--- test/express-engine.spec.ts
+++ test/express-engine.spec.ts
@@ -3,11 +3,41 @@ import * as Chai from "chai"
 import { ExpressEngine } from "../src/express-engine"
 import * as Express from "express"
 import * as Kamboja from "kamboja"
+import * as Lodash from "lodash"
+import * as Fs from "fs"
 
+let defaultOptions = <Kamboja.KambojaOption>{
+    skipAnalysis: false,
+    showConsoleLog: true,
+    controllerPaths: ["controller"],
+    modelPath: "model",
+    viewPath: "view",
+    staticFilePath: "public",
+    viewEngine: "hbs",
+    dependencyResolver: new Kamboja.DefaultDependencyResolver(new Kamboja.DefaultIdentifierResolver()),
+    identifierResolver: new Kamboja.DefaultIdentifierResolver(),
+}
+
+let facade: Kamboja.Facade = {
+    idResolver: new Kamboja.DefaultIdentifierResolver(),
+    resolver: new Kamboja.DefaultDependencyResolver(new Kamboja.DefaultIdentifierResolver()),
+    metadataStorage: new Kamboja.MetaDataStorage(new Kamboja.DefaultIdentifierResolver()),
+    validators: []
+}
 
 describe("ExpressEngine", () => {
-    it("Should init express properly", () => {
+    it.only("Should init express properly", () => {
         let engine = new ExpressEngine()
-        engine.init(null, {controllerPaths: ["test/harness/controller"]})
+        let options = Lodash.assign(defaultOptions, {
+            controllerPaths: ["test/harness/controller"]
+        })
+        let routeGen = new Kamboja.RouteGenerator(options.controllerPaths, facade, Fs.readFileSync)
+        let app = engine.init(routeGen.getRoutes(), options)
+        return Supertest(app)
+            .get("/user")
+            .expect((result) => {
+                console.log(result)
+            })
+            .expect(200)
     })
 })
