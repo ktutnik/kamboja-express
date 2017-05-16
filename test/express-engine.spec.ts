e1572bbc2b759ec4f1e0ca3d3aeed5dc52bddac3
Ketut Sandiarsa <ktutnik@gmail.com>
2017-05-16 17:36:14 +0800

    Fixing

--- test/express-engine.spec.ts
+++ test/express-engine.spec.ts
@@ -312,7 +312,7 @@ describe("Integration", () => {
                 .type("application/json")
                 .expect((result) => {
                     //console.log(result)
-                    Chai.expect(result.text).eq("Unexpected end")
+                    Chai.expect(result.text).contain("Unexpected end")
                 })
                 .expect(400)
         })
